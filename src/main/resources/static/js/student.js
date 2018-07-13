d1796db698f7e69ea2b5d99942e4c8593d713bf4
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2018-07-13 14:22:08 +0300

    small changes

--- src/main/resources/static/js/student.js
+++ src/main/resources/static/js/student.js
@@ -2,30 +2,39 @@
  * Created by andriusbaltrunas on 7/13/2018.
  */
 
+    var constant = new function(){
+    this.SAVE_URL = "/save";
+    this.GET_ALL_URL = "/all";
+    this.POST_METHOD = "POST";
+}
     var studentApp = angular.module("studentApp", []);
 
     studentApp.controller("studentController", function($scope, $http){
         $scope.alertType = "";
         $scope.alertMessage = "";
-        $http.get("/all").then(function(response){
-            $scope.students = response.data;
-        });
-
+        loadStudents($scope, $http);
         $scope.save = function(){
             var data = {name: $scope.name,
                 surname: $scope.surname,
                 phone : $scope.phone,
                 email : $scope.email};
             $http({
-                url :"/save",
-                method: "POST",
+                url :constant.SAVE_URL,
+                method: constant.POST_METHOD,
                 data: data
             }).then(function(response){
                 $scope.alertType = "alert-success";
                 $scope.alertMessage = "Student saved successfully";
+                loadStudents($scope, $http);
             }, function(response){
                 $scope.alertType = "alert-danger";
                 $scope.alertMessage = "Student do not saved!!!";
             });
         }
     });
+
+function loadStudents($scope, $http){
+    $http.get(constant.GET_ALL_URL).then(function(response){
+        $scope.students = response.data;
+    });
+}
